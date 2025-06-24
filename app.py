ea0ce66141f4e5f65ec3ade9b45b3b1a96946f70
Ubuntu <azureuser@flask.5ewezuh1nequdnkk5xurv2fodg.zx.internal.cloudapp.net>
2025-06-24 18:38:04 +0000

    changed files

--- app.py
+++ app.py
@@ -40,5 +40,5 @@ def submissions():
 if __name__ == '__main__':
     init_db()
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug-True, host='0.0.0.0', port=port)
 
