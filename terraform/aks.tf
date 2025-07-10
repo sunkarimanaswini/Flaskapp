3f039dca078f76a75f0e03ff2a5b5560f0dad09f
sunkarimanaswini <sunkari.manaswini19@gmail.com>
2025-07-10 19:00:47 +0100

    Update aks.tf

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -1,15 +1,3 @@
-resource "azurerm_resource_group" "example"{
-    name = var.resource_group_name
-    location = var.resource_group_location
-} 
-
-resource "azurerm_container_registry" "acr"{
-    name = var.container_registry_name
-    resource_group_name = azurerm_resource_group.example.name
-    location = azurerm_resource_group.example.location
-    sku = "Basic"
-    admin_enabled = "true"
-}
 resource "azurerm_kubernetes_cluster" "example" {
   name                = var.cluster_name
   location            = azurerm_resource_group.example.location
